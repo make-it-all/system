a383268aec4dd0110283de556c96e417fd886c32
Henry Morgan <henry@cd2solutions.co.uk>
2017-02-19 14:23:45 +0000

    bug

--- src/Application/View.php
+++ src/Application/View.php
@@ -144,21 +144,3 @@ class View {
   }
 
 }
-
-
-
-<?php $this->form_for($user, '/users'); ?>
-  <h1>New User</h1>
-  <?php $this->text_field($user, 'name'); ?>
-  <?php $this->text_field($user, 'email'); ?>
-  <?php $this->password_field($user, 'password'); ?>
-  <fieldset>
-    <legend>Role</legend>
-    <?php $this->checkbox_field($user, 'is_operator'); ?>
-    <?php $this->checkbox_field($user, 'is_specialist'); ?>
-    <?php $this->checkbox_field($user, 'is_admin'); ?>
-  </fieldset>
-  <div class="actions">
-    <?php $this->submit_field('Create User'); ?>
-  </div>
-</form>
